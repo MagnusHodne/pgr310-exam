3b0d1bba30483756d2fb7c6ef32cd7b68676f1dd
Magnus Hodne <60000396+MagnusHodne@users.noreply.github.com>
2022-12-04 14:46:22 +0100

    Created alarm for number of carts (#9)

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,20 @@
+resource "aws_cloudwatch_metric_alarm" "totalcarts" {
+  alarm_name          = "customer-boom"
+  namespace           = var.candidate_id
+  metric_name         = "carts.value"
+
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "5"
+  evaluation_periods  = "3"
+  period              = "300"
+
+  statistic           = "Maximum"
+
+  alarm_description   = "Alarm goes off if max number of carts in the last 3 five minute periods exceeds 5"
+  insufficient_data_actions = []
+  alarm_actions = [aws_sns_topic.alarms.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
